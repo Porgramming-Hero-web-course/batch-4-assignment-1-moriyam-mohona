24d4121b8d70e1526e5ca0829a0ab757c51ddff8
moriyam-mohona <moriyammohona@gmail.com>
2024-11-07 10:55:57 +0600

    setup

---
+++ dist/Problem-1.js
@@ -0,0 +1,13 @@
+"use strict";
+{
+    //
+    //     Write a TypeScript function sumArray that takes an array of numbers and returns the sum of all elements in the array.
+    // // Sample Input:
+    // sumArray([1, 2, 3, 4, 5]);
+    // // Sample Output:
+    // 15;
+    function sumArray(numbers) {
+        return numbers.reduce((acc, curr) => acc + curr, 0);
+    }
+    console.log(sumArray([1, 2, 3, 4, 5]));
+}
