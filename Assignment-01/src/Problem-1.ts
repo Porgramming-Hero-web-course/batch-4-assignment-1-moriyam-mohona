8b2429fd09297681700d5ce043162985bed1f9df
moriyam-mohona <moriyammohona@gmail.com>
2024-11-07 11:33:07 +0600

    Prblem-02

--- Assignment-01/src/Problem-1.ts
+++ Assignment-01/src/Problem-1.ts
@@ -1,12 +1,7 @@
 {
-  //
-  //     Write a TypeScript function sumArray that takes an array of numbers and returns the sum of all elements in the array.
-
-  // // Sample Input:
-  // sumArray([1, 2, 3, 4, 5]);
-
-  // // Sample Output:
-  // 15;
+  // Write a TypeScript function sumArray that takes an array of numbers and returns the sum of all elements in the array.
+  //Input: sumArray([1, 2, 3, 4, 5]);
+  //Output: 15
 
   function sumArray(numbers: number[]): number {
     return numbers.reduce((acc, curr) => acc + curr, 0);
--- Assignment-01/src/Problem-2.ts
+++ Assignment-01/src/Problem-2.ts
@@ -1,2 +1,10 @@
-const nname: string = "Nishi";
-console.log(nname);
+{
+  // Create a TypeScript function removeDuplicates that accepts an array of numbers and returns a new array with duplicates removed. Preserve the original order of elements.
+  // Input: removeDuplicates([1, 2, 2, 3, 4, 4, 5])
+  // Output: [1, 2, 3, 4, 5]
+
+  function removeDuplicates(numbers: number[]): number[] {
+    return numbers.filter((num, index) => numbers.indexOf(num) === index);
+  }
+  console.log(removeDuplicates([1, 2, 2, 3, 3, 3, 4, 4, 5]));
+}
